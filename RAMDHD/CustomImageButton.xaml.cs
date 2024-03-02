23e9a1ec6d12bcb1cfdaa4a746a04ab31abac49e
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-02 10:41:52 +0100

    refactor(CustomImageButton): Add CustomImageButton reusable element
    CustomImageButton is a generic element that abstracts Frame component and provides properties allowing to create Image+Text button that calls for a given method

---
+++ RAMDHD/CustomImageButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace RAMDHD
+{
+    public partial class CustomImageButton : ContentView
+    {
+        public static readonly BindableProperty ImageSourceProperty =
+            BindableProperty.Create(nameof(ImageSource), typeof(ImageSource), typeof(CustomImageButton), default(ImageSource));
+
+        public static readonly BindableProperty ButtonTextProperty =
+            BindableProperty.Create(nameof(ButtonText), typeof(string), typeof(CustomImageButton), default(string));
+
+        public event EventHandler Tapped;
+
+        public CustomImageButton()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void OnFrameTapped(object sender, EventArgs e)
+        {
+            // Invoke any event handlers attached to our custom event
+            Tapped?.Invoke(this, e);
+        }
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+
+        public string ButtonText
+        {
+            get => (string)GetValue(ButtonTextProperty);
+            set => SetValue(ButtonTextProperty, value);
+        }
+    }
+}
