379fc03992885ee307833c9d40be6539daf63004
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-13 14:10:17 +0100

    style/feat(TitleScreen): Add TitleScreen presenting the app title and 'logo'
    Clicking on the brainLogo redirects to the HomeScreen

---
+++ RAMDHD/TitleScreen.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace RAMDHD
+{
+    public partial class TitleScreen : ContentPage
+    {
+        public TitleScreen()
+        {
+            InitializeComponent(); // This is necessary.
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += async (s, e) =>
+            {
+                // Use the built-in navigation mechanism if inside a NavigationPage or Shell.
+                await this.Navigation.PushAsync(new MainPage());
+            };
+
+            // Assuming you have an image with x:Name="BrainImage" in your TitleScreen.xaml
+            BrainImage.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+    }
+}
