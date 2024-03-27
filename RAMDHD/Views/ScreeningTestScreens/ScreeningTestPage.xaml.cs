d5a25d26898abe442e97d88f4197be8531419a6f
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-27 11:08:01 +0100

    refactor/style(ScreeningTestPage): Refactor ScreeningTestPage for MVVM and Navigation
    - Bind RadioButton IsChecked to ViewModel.
    - Bind Next Question button text to ViewModel's NextButtonText.
    - Implement MessagingCenter subscription for navigation to ShowResultsPage.
    - Handle RadioButton CheckedChanged to update ViewModel's SelectedAnswerScore. (NOT WORKING FOR SOME REASON)
    - Unsubscribe from MessagingCenter on page disappearance to prevent memory leaks.

--- RAMDHD/Views/ScreeningTestScreens/ScreeningTestPage.xaml.cs
+++ RAMDHD/Views/ScreeningTestScreens/ScreeningTestPage.xaml.cs
@@ -12,6 +12,13 @@ namespace RAMDHD.Views.ScreeningTestScreens
             InitializeComponent();
             _viewModel = new ScreeningTestViewModel();
             this.BindingContext = _viewModel;
+
+            // Subscribe to the message sent from the ViewModel to navigate to the results page
+            MessagingCenter.Subscribe<ScreeningTestViewModel, string>(this, "NavigateToShowResultsPage", async (sender, result) =>
+            {
+                // Assume ShowResultsPage is the page you want to navigate to, and it has a constructor that accepts a result string
+                await Navigation.PushAsync(new ShowResultsPage(result));
+            });
         }
 
         private void OnAnswerChanged(object sender, CheckedChangedEventArgs e)
@@ -21,8 +28,19 @@ namespace RAMDHD.Views.ScreeningTestScreens
                 var viewModel = (ViewModels.ScreeningTest.ScreeningTestViewModel)BindingContext;
                 var selectedAnswerIndex = viewModel.CurrentQuestion.Answers.IndexOf(radioButton.Content.ToString());
                 viewModel.CurrentQuestion.SelectedAnswerScore = selectedAnswerIndex;
+                // Assuming the Content of the RadioButton is the answer text
+                string answerText = radioButton.Value.ToString();
+
+                // Call a method on the ViewModel to handle the selection
+                _viewModel.SelectAnswer(answerText);
             }
         }
-    }
 
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            // Unsubscribe from the MessagingCenter to prevent memory leaks
+            MessagingCenter.Unsubscribe<ScreeningTestViewModel, string>(this, "NavigateToShowResultsPage");
+        }
+    }
 }
