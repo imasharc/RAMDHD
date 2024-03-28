354d6cf2cbcc5545aa673fe9689067dc76f0b552
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-28 09:48:35 +0100

    refactor/feat(Question): Enhance Question model with property change notifications - TO BE REVIEWED BC DOESN'T WORK AS EXPECTED
    - Implement INotifyPropertyChanged in Question class for UI data binding support.
    - Add SelectedAnswerIndex property to track the index of selected answers.
    - Introduce IsChecked property with change notification to track question selection state.
    - Change the initialization of SelectedAnswerScore from -1 to 0, adjusting the default answer score.
    - Implement OnPropertyChanged method to facilitate property change events.

--- RAMDHD/Models/ScreeningTest/Question.cs
+++ RAMDHD/Models/ScreeningTest/Question.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RAMDHD.Models.ScreeningTest
 {
-    public class Question
+    public class Question : INotifyPropertyChanged
     {
         public string Text { get; set; }
         public List<string> Answers { get; set; }
-        public int SelectedAnswerScore { get; set; } = -1; // -1 means no answer selected
+        public int SelectedAnswerScore { get; set; } = 0;
+        public int SelectedAnswerIndex { get; set; } = -1; // Default to -1 indicating no selection
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set
+            {
+                if (_isChecked != value)
+                {
+                    _isChecked = value;
+                    OnPropertyChanged(nameof(IsChecked));
+                }
+            }
+        }
+
+        // Implement INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
