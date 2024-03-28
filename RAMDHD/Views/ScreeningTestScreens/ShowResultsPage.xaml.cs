833ad3df7f09c3cce5a2a518fc0b5c5c70073bf3
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-28 17:14:38 +0100

    feat(ShowResultsPage): Introduce ShowResultsPage for Screening Test Outcomes
    - Created ShowResultsPage to display the results of the ADHD screening test, enriching user feedback and engagement.
    - Designed a multi-section layout using Grid, enhancing the structured presentation of test outcomes, an illustrative icon (HARDCODED FOR NOW - WILL SOON BE DEPENDENT ON THE RESULTS), and a navigation button.
    - Implemented dynamic result display through a Label, providing personalized feedback to users based on their screening test responses.
    - Incorporated an illustrative icon related to ADHD awareness, reinforcing the thematic focus of the test results.
    - Added a 'Start using the app' Button, facilitating a seamless transition for users to explore the app further post-test.
    - Embedded navigation logic within the button click event, ensuring users are guided to the HomePage, thus improving the app's navigational flow.
    - Ensured clean event handler management by subscribing and unsubscribing to the button's Clicked event, preventing potential memory leaks and enhancing app performance.

---
+++ RAMDHD/Views/ScreeningTestScreens/ShowResultsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+using RAMDHD.Views.MainScreens;
+using System;
+
+namespace RAMDHD.Views.ScreeningTestScreens
+{
+    public partial class ShowResultsPage : ContentPage
+    {
+        public ShowResultsPage(string result)
+        {
+            InitializeComponent();
+            ResultLabel.Text = $"Your ADHD Type: {result}";
+
+            // Subscribe to the Clicked event of the button
+            StartUsingAppButton.Clicked += StartUsingAppButton_Clicked;
+        }
+
+        private async void StartUsingAppButton_Clicked(object sender, EventArgs e)
+        {
+            // Navigate to the HomePage
+            await Navigation.PushAsync(new HomePage());
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            // Unsubscribe from the Clicked event to prevent memory leaks
+            StartUsingAppButton.Clicked -= StartUsingAppButton_Clicked;
+        }
+    }
+}
