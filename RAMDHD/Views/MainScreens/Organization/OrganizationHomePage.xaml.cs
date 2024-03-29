94282d8f8e3019bcec9b8bb84bccfd08cdfae3b0
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-29 09:55:51 +0100

    style/feat(OrganizationHomePage): Add Organization HomePage with Navigation and Styling
    STYLE:
    - Introduced a Frame at the top of the page as a clickable header, adorned with a custom border and containing an image set to fill the available space.
    - Implemented VerticalStackLayout for vertical alignment and centering of page elements, enhancing the overall layout structure.
    - Added a non-interactive Button to act as a category indicator for the page, styled with specific dimensions and colors to match the theme.
    - Designed a Grid layout for organizational tools, each represented by a Button with unique background and text colors, currently set to non-interactive mode.
    - Included a bottom navigation bar with a home icon and placeholder buttons for different categories, laying the groundwork for future navigation enhancements.
    
    FUNCTIONALITY:
    - Enabled tapping on the top image, with a placeholder event handler 'OnGraphTasksClicked' ready for future implementation.
    - Configured the 'Attention' category button to navigate to the 'AttentionHomePage', using page insertion and stack popping for navigation flow.
    - Set up the home icon with 'OnHomeImageTapped' event handler to allow users to navigate back to the HomePage, enhancing user navigation experience.
    
    This update introduces a foundational layout and navigation framework for the OrganizationHomePage, setting the stage for further development and integration of specific functionalities.

---
+++ RAMDHD/Views/MainScreens/Organization/OrganizationHomePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using RAMDHD.Views.MainScreens.Attention;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAMDHD.Views.MainScreens.Organization
+{
+    public partial class OrganizationHomePage : ContentPage
+    {
+        public OrganizationHomePage()
+        {
+            InitializeComponent();
+        }
+        private void OnGraphTasksClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnGraphTasksClicked");
+        }
+        private async void OnAttentionClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnAttentionClicked");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new AttentionHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnHomeImageTapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnHomeImageTapped");
+            // Navigate to the home page
+            await Navigation.PopAsync();
+
+            //// Navigate to the home page using Shell navigation
+            //await Shell.Current.GoToAsync("//HomePage");
+        }
+    }
+}
