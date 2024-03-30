224fdfe67355ec2c06f6dac093cfdc337416cf96
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-30 08:03:17 +0100

    style/feat(PeopleHomePage): Add People Category Page with Navigation and Style Updates
    - **Style:**
      - Created a new People category page with a consistent LightBlue background theme.
      - Implemented a top view with an interactive header, styled with a black border, rounded corners, and featuring an aspect-filled image.
      - Added a non-interactive category indicator for 'People' with customized styling to signify the current page.
      - Designed a grid layout for feature buttons with distinct colors, all disabled to serve as placeholders for future functionality.
      - Established a bottom navigation bar with home and category navigation buttons, applying unique styling to denote the active category and facilitate user navigation.
    
    - **Functionality:**
      - Enabled navigation from the People page to the Attention, Organization, and Entertainment categories through the bottom navigation bar, ensuring a fluid user experience across different sections of the app.
      - Configured the home icon to return users to the HomePage, enhancing the app's navigational flow.
      - Applied Navigation.InsertPageBefore and Navigation.PopAsync methods for smooth transitions between category pages, maintaining a clean navigation stack.
    
    This update introduces the People category, enriching the app's content and improving navigational coherence. Future updates will activate the feature buttons and expand the page's functionality.

---
+++ RAMDHD/Views/MainScreens/People/PeopleHomePage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using RAMDHD.Views.MainScreens.Attention;
+using RAMDHD.Views.MainScreens.Entertainment;
+using RAMDHD.Views.MainScreens.Organization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAMDHD.Views.MainScreens.People
+{
+    public partial class PeopleHomePage : ContentPage
+    {
+        public PeopleHomePage()
+        {
+            InitializeComponent();
+        }
+        private async void OnAttentionClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnAttentionClicked");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new AttentionHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnOrganizationClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnOrganizationClicked");
+            // Pop the current page off the stack
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new OrganizationHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnEntertainmentClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnEntertainmentClicked");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new EntertainmentHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnHomeImageTapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnHomeImageTapped");
+            // Navigate to the home page
+            await Navigation.PopAsync();
+
+            //// Navigate to the home page using Shell navigation
+            //await Shell.Current.GoToAsync("//HomePage");
+        }
+    }
+}
