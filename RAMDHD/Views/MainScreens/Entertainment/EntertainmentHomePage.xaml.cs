c6fff45593a4b4a1f062ae1877fa8e99ebf4412d
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-30 08:07:32 +0100

    style/feat(EntertainmentHomePage): Implement Entertainment Category with Navigation and Styling
    - **Style:**
      - Added Entertainment category page with a LightBlue background for consistency across the app.
      - Designed a clickable header at the top, framed with a black border and rounded corners, showcasing an aspect-fill image.
      - Integrated a non-interactive button to indicate the 'ROZRYWKA' category, utilizing a white background and grey text to highlight the current page.
      - Structured a grid layout for feature buttons like 'AFIRMACJE', 'MANDALE', and a placeholder (for keeping the bottom navigation AT THE BOTTOM - Grid structure is kept), each with unique background colors but set to disabled as placeholders for future features.
      - Established a bottom navigation grid, aligning with the app's design language, to facilitate user navigation across categories and back to the HomePage.
    
    - **Functionality:**
      - Enabled smooth transition to Attention, Organization, and People categories through the bottom navigation, enhancing app navigation.
      - Configured the home icon to redirect users to the HomePage, ensuring a consistent navigational flow.
      - Utilized Navigation.InsertPageBefore and Navigation.PopAsync methods for seamless navigation between categories without cluttering the navigation stack.
    
    This commit enriches the app with an Entertainment section, improving its visual appeal and user navigation. Future updates will activate the feature buttons, adding depth to the Entertainment category.

---
+++ RAMDHD/Views/MainScreens/Entertainment/EntertainmentHomePage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using RAMDHD.Views.MainScreens.Attention;
+using RAMDHD.Views.MainScreens.Organization;
+using RAMDHD.Views.MainScreens.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAMDHD.Views.MainScreens.Entertainment
+{
+    public partial class EntertainmentHomePage : ContentPage
+    {
+        public EntertainmentHomePage()
+        {
+            InitializeComponent();
+        }
+        private void OnGraphTasksClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnGraphTasksClicked");
+        }
+        private async void OnAttentionClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnAttentionClicked");
+            // Pop the current page off the stack
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new AttentionHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnOrganizationClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OrganizationHomePage");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new OrganizationHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnPeopleClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnPeopleClicked");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new PeopleHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnHomeImageTapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnHomeImageTapped");
+            // Navigate to the home page
+            await Navigation.PopAsync();
+
+            //// Navigate to the home page using Shell navigation
+            //await Shell.Current.GoToAsync("//HomePage");
+        }
+    }
+}
