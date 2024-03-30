33e0007e664d56b52e198955dcf0677835f0004c
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-03-30 08:15:52 +0100

    style/feat(OrganizationHomePage): Refine OrganizationHomePage with Interactive UI and Enhanced Navigation
    - **Style:**
      - Structured the page layout with a clear visual hierarchy, incorporating a top view/header, category indicator, button grid, and bottom navigation for a coherent user interface.
      - Adopted a green color scheme for the category indicator and buttons to represent the 'Organization' category distinctly.
      - Styled buttons within the grid with specific background and text colors, currently set as placeholders with IsEnabled='False'.
    
    - **Functionality:**
      - Integrated TapGestureRecognizer for the top view/header linked to OnGraphTasksClicked for potential future interactions.
      - Implemented bottom navigation with category buttons, facilitating seamless navigation to 'Attention', 'People', and 'Entertainment' pages, with corresponding event handlers (OnAttentionClicked, OnPeopleClicked, OnEntertainmentClicked).
      - Utilized a combination of Navigation.InsertPageBefore and Navigation.PopAsync in navigation methods to ensure direct and seamless page transitions without navigating back through the current page.
    
    This update significantly enhances the OrganizationHomePage's interactive elements and navigational flow, setting a foundation for future functionalities and user engagement.

--- RAMDHD/Views/MainScreens/Organization/OrganizationHomePage.xaml.cs
+++ RAMDHD/Views/MainScreens/Organization/OrganizationHomePage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using RAMDHD.Views.MainScreens.Attention;
+using RAMDHD.Views.MainScreens.Entertainment;
+using RAMDHD.Views.MainScreens.People;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +22,27 @@ namespace RAMDHD.Views.MainScreens.Organization
         private async void OnAttentionClicked(object sender, EventArgs e)
         {
             Console.WriteLine("OnAttentionClicked");
+            // Pop the current page off the stack
             // Insert the new page before the current one
             Navigation.InsertPageBefore(new AttentionHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnPeopleClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnPeopleClicked");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new PeopleHomePage(), this);
+
+            // Pop the current page off the stack
+            await Navigation.PopAsync();
+        }
+        private async void OnEntertainmentClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnEntertainmentClicked");
+            // Insert the new page before the current one
+            Navigation.InsertPageBefore(new EntertainmentHomePage(), this);
 
             // Pop the current page off the stack
             await Navigation.PopAsync();
