8f843e96bdae2970d9f53578f4b304197bd83624
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-04-30 11:35:30 +0200

    feat/style(BuyingBreadGraphTask): Introduce sample graph task of Buying Bread
    ### Style:
    - **Dynamic Layout Configuration**: Utilizes an AbsoluteLayout to provide precise control over the placement of UI elements, which is crucial for presenting the sequential steps of the task visually.
    - **Thematic Consistency and Appeal**: Each activity is encapsulated within a styled Frame with specific BackgroundColor to denote the status or type of the task. Notably, the first task uses an Orange background to indicate its procrastination potential.
    - **Icon Integration**: Each step of the task is represented visually with icons like phone.png, shopping_list.png, and jacket_shoes.png, enhancing understanding and engagement through visual cues.
    - **Informational Design**: The layout includes detailed labels next to each step, providing explicit instructions or notes related to the task, enhancing clarity and user guidance.
    - **Navigational Aids**: Curved arrow images guide the user's eye through the task sequence, reinforcing the flow of activities visually.
    
    ### Functionality Enhancements:
    - **Interactive Elements**: The frames and labels include tap gestures that potentially could be linked to more detailed explanations or modifications of each task, adding a layer of interactivity.
    - **Central Navigation Button**: A prominent button is styled and positioned to guide users back to the main menu of graph tasks, ensuring easy navigation and accessibility.
    - **Bottom Navigation Bar**: Incorporates a fully interactive bottom navigation grid, enabling quick access to various sections of the app such as attention, organization, and relaxation, facilitating user movement within the app and enhancing the overall user experience.
    - **Contextual Actions**: The bottom navigation also includes gesture recognizers for common actions like returning to the graph task menu or accessing other main screens, which reinforces the app's navigational structure and ease of use.
    
    These stylistic and functional enhancements make the BuyingBreadGraphTask page not only visually appealing but also highly user-friendly, promoting an intuitive flow through task management activities.

---
+++ RAMDHD/Views/MainScreens/GraphTask/Examples/BuyingBreadGraphTask.xaml.cs
@@ -0,0 +1,54 @@
+﻿using RAMDHD.Views.MainScreens.Attention;
+using RAMDHD.Views.MainScreens.Entertainment;
+using RAMDHD.Views.MainScreens.Organization;
+using RAMDHD.Views.MainScreens.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAMDHD.Views.MainScreens.GraphTask.Examples
+{
+    public partial class BuyingBreadGraphTask : ContentPage
+    {
+        public BuyingBreadGraphTask()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateToGraphTaskMenu(object sender, EventArgs e)
+        {
+            // Navigate to GraphTaskMenu
+            await this.Navigation.PushAsync(new GraphTaskMenu());
+        }
+        private async void OnAttentionClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnAttentionClicked");
+            // Navigate to AttentionHomePage
+            await this.Navigation.PushAsync(new AttentionHomePage());
+        }
+        private async void OnOrganizationClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnOrganizationClicked");
+            // Navigate to OrganizationHomePage
+            await this.Navigation.PushAsync(new OrganizationHomePage());
+        }
+        private async void OnPeopleClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnAttentionClicked");
+            // Navigate to PeopleHomePage
+            await this.Navigation.PushAsync(new PeopleHomePage());
+        }
+        private async void OnEntertainmentClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("OnEntertainmentClicked");
+            // Navigate to EntertainmentHomePage
+            await this.Navigation.PushAsync(new EntertainmentHomePage());
+        }
+        private async void OnGraphTasksClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("GraphTasks");
+            await this.Navigation.PushAsync(new ProcrastinationHomePage());
+        }
+    }
+}
