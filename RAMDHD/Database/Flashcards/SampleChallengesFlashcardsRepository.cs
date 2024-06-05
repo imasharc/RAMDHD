7b0671f9e6bec51db454b64f1e15571de496bb33
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-06-05 15:03:42 +0200

    style/feat(SampleChallengesFlashcardsPage): Add page that displays challenging flashcards
    User can flip the flashcards (by tapping on it) to reveal the other side
    
    User can also draw a random flashcard from the premade stack

---
+++ RAMDHD/Database/Flashcards/SampleChallengesFlashcardsRepository.cs
@@ -0,0 +1,27 @@
+﻿using RAMDHD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAMDHD.Database.Flashcards
+{
+    class SampleChallengesFlashcardsRepository
+    {
+        public static List<Flashcard> GetFlashcards()
+        {
+            return new List<Flashcard>
+            {
+                new Flashcard { Question = "I struggle to stay focused...", Answer = "Set a timer for 5 minutes and work on a task without distraction.\n\nReward yourself afterwards!" },
+                new Flashcard { Question = "I often feel restless...", Answer = "Get up and do 60 seconds of push-ups/squats/jumping-jacks" },
+                new Flashcard { Question = "I have difficulty starting tasks...", Answer = "Try performing a task for just 2 minutes. Observe yourself afterwards." },
+                new Flashcard { Question = "I stuggle to start a conversation...", Answer = "Go to a local bakery and make a small talk with cashier eg. comment on the weather." },
+                new Flashcard { Question = "I struggle with remembering what people have asked me for...", Answer = "As soon as you get asked to do something, write it down in the notes." },
+                new Flashcard { Question = "I struggle with biting my nails...", Answer = "Find a simple fidget toy eg. a ruber band or a keychain. You can buy one as well." },
+                new Flashcard { Question = "I feel down when I am not productive during the day...", Answer = "Whatever happens during a day, perform some leisure physical activity in the afternoon. Try going for a bike or a walk to wind out after a hard day." },
+                // Add more flashcards as needed
+            };
+        }
+    }
+}
---
+++ RAMDHD/Views/MainScreens/Attention/Flashcards/Challenging/Sample/SampleChallengesFlashcardsPage.xaml.cs
@@ -0,0 +1,124 @@
+using RAMDHD.Database.Flashcards;
+using RAMDHD.Models;
+using RAMDHD.Views.MainScreens.Mindfulness;
+using RAMDHD.Views.MainScreens.GraphTask;
+using RAMDHD.Views.MainScreens.Organization;
+using RAMDHD.Views.MainScreens.People;
+using System.Windows.Input;
+
+namespace RAMDHD.Views.MainScreens.Attention.Flashcards.Challenging.Sample;
+
+public partial class SampleChallengesFlashcardsPage : ContentPage
+{
+    private int activePage;
+    private bool isFlashcardFlipped = false;
+    private List<Flashcard> flashcards;
+    private Random random = new Random();
+    private const uint rotationDuration = 130; // Adjusted rotation duration
+
+    public SampleChallengesFlashcardsPage()
+    {
+        ActivePage = 0;
+        InitializeComponent();
+        BindingContext = this;
+        LoadFlashcards();
+    }
+    public int ActivePage
+    {
+        get => activePage;
+        set
+        {
+            if (activePage != value)
+            {
+                activePage = value;
+                OnPropertyChanged(nameof(ActivePage));
+            }
+        }
+    }
+    public ICommand NavigateCommand => new Command<string>(async (page) =>
+    {
+        switch (page)
+        {
+            case "Attention":
+                await this.Navigation.PushAsync(new AttentionHomePage());
+                break;
+            case "Organization":
+                await this.Navigation.PushAsync(new OrganizationHomePage());
+                break;
+            case "People":
+                await this.Navigation.PushAsync(new PeopleHomePage());
+                break;
+            case "Mindfulness":
+                await this.Navigation.PushAsync(new MindfulnessHomePage());
+                break;
+            case "GraphTasks":
+                await this.Navigation.PushAsync(new ProcrastinationHomePage());
+                break;
+        }
+    });
+    private void LoadFlashcards()
+    {
+        flashcards = SampleChallengesFlashcardsRepository.GetFlashcards();
+        if (flashcards.Count > 0)
+        {
+            DisplayFlashcard(flashcards[0]);
+        }
+    }
+
+    private void DisplayFlashcard(Flashcard flashcard)
+    {
+        Flashcard1Front.Text = flashcard.Question;
+        Flashcard1Back.Text = flashcard.Answer;
+    }
+
+    private async void OnFlashcard1Tapped(object sender, EventArgs e)
+    {
+        if (isFlashcardFlipped)
+        {
+            await FlipToFront(Flashcard1, Flashcard1Front, Flashcard1Back);
+        }
+        else
+        {
+            await FlipToBack(Flashcard1, Flashcard1Front, Flashcard1Back);
+        }
+        isFlashcardFlipped = !isFlashcardFlipped;
+    }
+
+    private async Task FlipToFront(View flashcard, View front, View back)
+    {
+        Flashcard1.HasShadow = false; // Remove shadow during flip
+        await flashcard.RotateYTo(90, rotationDuration);
+        back.IsVisible = false;
+        flashcard.BackgroundColor = Colors.White; // Change to front color
+
+        front.IsVisible = true;
+        flashcard.RotationY = -90;
+        await flashcard.RotateYTo(0, rotationDuration);
+        Flashcard1.HasShadow = true; // Restore shadow after flip
+    }
+
+    private async Task FlipToBack(View flashcard, View front, View back)
+    {
+        Flashcard1.HasShadow = false; // Remove shadow during flip
+        await flashcard.RotateYTo(90, rotationDuration);
+        front.IsVisible = false;
+        flashcard.BackgroundColor = Colors.LightGreen; // Change to front color
+
+        back.IsVisible = true;
+        flashcard.RotationY = -90;
+        await flashcard.RotateYTo(0, rotationDuration);
+        Flashcard1.HasShadow = true; // Restore shadow after flip
+    }
+
+    private async void OnRandomButtonClicked(object sender, EventArgs e)
+    {
+        if (isFlashcardFlipped)
+        {
+            await FlipToFront(Flashcard1, Flashcard1Front, Flashcard1Back);
+            isFlashcardFlipped = false;
+        }
+
+        var randomIndex = random.Next(flashcards.Count);
+        DisplayFlashcard(flashcards[randomIndex]);
+    }
+}
