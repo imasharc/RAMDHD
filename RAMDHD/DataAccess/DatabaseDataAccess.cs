0f04a646175a97bf81dc2b4095af4ebe5038bad9
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-06-05 09:28:37 +0200

    feat(DatabaseDataAccess): Add database controller for Notes and Routines

--- RAMDHD/DataAccess/DatabaseDataAccess.cs
+++ RAMDHD/DataAccess/DatabaseDataAccess.cs
@@ -1,11 +1,5 @@
-﻿using Microsoft.Maui.Controls;
-using RAMDHD.Database;
+﻿using RAMDHD.Database;
 using RAMDHD.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RAMDHD.DataAccess
 {
@@ -56,7 +50,22 @@ namespace RAMDHD.DataAccess
 
             transaction.Commit();
         }
+        public async Task<bool> DeleteGraphTaskByIdAsync(int id)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
 
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+        DELETE FROM GraphTask
+        WHERE Id = $Id
+    ";
+            command.Parameters.AddWithValue("$Id", id);
+
+            var affectedRows = await command.ExecuteNonQueryAsync();
+
+            return affectedRows > 0; // Return true if at least one row was deleted
+        }
         public async Task<GraphTask> GetGraphTaskByIdAsync(int id)
         {
             using var connection = _dbConnection.GetDbConnection();
@@ -119,6 +128,222 @@ namespace RAMDHD.DataAccess
             }
 
             return graphTasks;
+        }
+
+        public async Task InsertOrUpdateNoteByIdAsync(Note note, int? id = null)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            using var transaction = connection.BeginTransaction();
+
+            var insertCommand = connection.CreateCommand();
+
+            // If an ID is provided, use it; otherwise, AUTOINCREMENT will handle it
+            if (id.HasValue)
+            {
+                insertCommand.CommandText = @"
+        INSERT OR REPLACE INTO Note (Id, Headline, Description)
+        VALUES ($Id, $Headline, $Description)
+        ";
+                insertCommand.Parameters.AddWithValue("$Id", id.Value);
+            }
+            else
+            {
+                insertCommand.CommandText = @"
+        INSERT OR REPLACE INTO Note (Id, Headline, Description)
+        VALUES ($Id, $Headline, $Description)
+        ";
+            }
+
+            insertCommand.Parameters.AddWithValue("$Headline", note.Headline);
+            insertCommand.Parameters.AddWithValue("$Description", note.Description);
+
+            await insertCommand.ExecuteNonQueryAsync();
+
+            transaction.Commit();
+        }
+        public async Task<bool> DeleteNoteByIdAsync(int id)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+        DELETE FROM Note
+        WHERE Id = $Id
+    ";
+            command.Parameters.AddWithValue("$Id", id);
+
+            var affectedRows = await command.ExecuteNonQueryAsync();
+
+            return affectedRows > 0; // Return true if at least one row was deleted
+        }
+        public async Task<Note> GetNoteByIdAsync(int id)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+        SELECT Id, Headline, Description
+        FROM Note
+        WHERE Id = $Id
+    ";
+            command.Parameters.AddWithValue("$Id", id);
+
+            using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                var note = new Note
+                {
+                    Id = reader.GetInt32(0),
+                    Headline = reader.GetString(1),
+                    Description = reader.GetString(2)
+                };
+                return note;
+            }
+
+            return null; // Return null or handle it if no task is found
+        }
+
+        public async Task<List<Note>> GetAllNotesAsync()
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+        SELECT Id, Headline, Description
+        FROM Note
+    ";
+
+            var notes = new List<Note>();
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var note = new Note
+                {
+                    Id = reader.GetInt32(0),
+                    Headline = reader.GetString(1),
+                    Description = reader.GetString(2)
+                };
+                notes.Add(note);
+            }
+
+            return notes;
+        }
+
+        public async Task InsertOrUpdateRoutineByIdAsync(Routine routine, int? id = null)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            using var transaction = connection.BeginTransaction();
+
+            var insertCommand = connection.CreateCommand();
+
+            // If an ID is provided, use it; otherwise, AUTOINCREMENT will handle it
+            if (id.HasValue)
+            {
+                insertCommand.CommandText = @"
+        INSERT OR REPLACE INTO Routine (Id, Title, Description, Step1, Step2, Step3, Step4)
+        VALUES ($Id, $Title, $Description, $Step1, $Step2, $Step3, $Step4)
+        ";
+                insertCommand.Parameters.AddWithValue("$Id", id.Value);
+            }
+            else
+            {
+                insertCommand.CommandText = @"
+        INSERT OR REPLACE INTO Routine (Id, Title, Description, Step1, Step2, Step3, Step4)
+        VALUES ($Id, $Title, $Description, $Step1, $Step2, $Step3, $Step4)
+        ";
+            }
+
+            insertCommand.Parameters.AddWithValue("$Title", routine.Title);
+            insertCommand.Parameters.AddWithValue("$Description", routine.Description);
+            insertCommand.Parameters.AddWithValue("$Step1", routine.Step1);
+            insertCommand.Parameters.AddWithValue("$Step2", routine.Step2);
+            insertCommand.Parameters.AddWithValue("$Step3", routine.Step3);
+            insertCommand.Parameters.AddWithValue("$Step4", routine.Step4);
+
+            await insertCommand.ExecuteNonQueryAsync();
+
+            transaction.Commit();
+        }
+        public async Task<bool> DeleteRoutineByIdAsync(int id)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+        DELETE FROM Routine
+        WHERE Id = $Id
+    ";
+            command.Parameters.AddWithValue("$Id", id);
+
+            var affectedRows = await command.ExecuteNonQueryAsync();
+
+            return affectedRows > 0; // Return true if at least one row was deleted
+        }
+        public async Task<Routine> GetRoutineByIdAsync(int id)
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+                SELECT Id, Title, Description, Step1, Step2, Step3, Step4
+                FROM Routine
+                WHERE Id = $Id
+            ";
+            command.Parameters.AddWithValue("$Id", id);
+
+            using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                var routine = new Routine
+                {
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    Description = reader.GetString(2),
+                    Step1 = reader.GetString(3),
+                    Step2 = reader.GetString(4),
+                    Step3 = reader.GetString(5),
+                    Step4 = reader.GetString(6),
+
+                };
+                return routine;
+            }
+
+            return null; // Return null or handle it if no task is found
+        }
+        public async Task<List<Routine>> GetAllRoutinesAsync()
+        {
+            using var connection = _dbConnection.GetDbConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"
+        SELECT Id, Title, Description
+        FROM Routine
+    ";
+
+            var routines = new List<Routine>();
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var routine = new Routine
+                {
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    Description = reader.GetString(2)
+                };
+                routines.Add(routine);
+            }
+
+            return routines;
         }
     }
 }
