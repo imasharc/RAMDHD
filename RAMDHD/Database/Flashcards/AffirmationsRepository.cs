4700f6f967c94b1ac89169eb01266fcff0516e5e
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-06-06 14:17:06 +0200

    style/feat(AffirmationsPage): Add page that displays affirmations
    User can draw a random affirmation from the premade stack

---
+++ RAMDHD/Database/Flashcards/AffirmationsRepository.cs
@@ -0,0 +1,30 @@
+﻿using RAMDHD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAMDHD.Database.Flashcards
+{
+    class AffirmationsRepository
+    {
+        public static List<Flashcard> GetFlashcards()
+        {
+            return new List<Flashcard>
+            {
+                new Flashcard { Question = "I embrace my unique way of thinking and know that it is a strength." },
+                new Flashcard { Question = "I am patient with myself as I navigate through my tasks and challenges." },
+                new Flashcard { Question = "I have the power to focus and accomplish my goals, one step at a time." },
+                new Flashcard { Question = "I forgive myself for past mistakes and celebrate my progress." },
+                new Flashcard { Question = "I am creative and capable, and my ADHD is a part of what makes me special." },
+                new Flashcard { Question = "I am resilient and can overcome any obstacles that come my way." },
+                new Flashcard { Question = "I celebrate small victories and acknowledge my efforts each day." },
+                new Flashcard { Question = "I am in control of my actions and can direct my energy positively." },
+                new Flashcard { Question = "I am worthy of understanding, patience, and compassion from myself and others." },
+
+                // Add more flashcards as needed
+            };
+        }
+    }
+}
---
+++ RAMDHD/Views/MainScreens/Mindfulness/Affirmations/AffirmationsPage.xaml.cs
@@ -0,0 +1,76 @@
+using RAMDHD.Database.Flashcards;
+using RAMDHD.Models;
+using RAMDHD.Views.MainScreens.GraphTask;
+using RAMDHD.Views.MainScreens.Organization;
+using RAMDHD.Views.MainScreens.People;
+using RAMDHD.Views.MainScreens.Attention;
+using System.Windows.Input;
+
+namespace RAMDHD.Views.MainScreens.Mindfulness.Affirmations;
+
+public partial class AffirmationsPage : ContentPage
+{
+    private int activePage;;
+    private List<Flashcard> flashcards;
+    private Random random = new Random();
+
+    public AffirmationsPage()
+    {
+        ActivePage = 0;
+        InitializeComponent();
+        BindingContext = this;
+        LoadFlashcards();
+    }
+    public int ActivePage
+    {
+        get => activePage;
+        set
+        {
+            if (activePage != value)
+            {
+                activePage = value;
+                OnPropertyChanged(nameof(ActivePage));
+            }
+        }
+    }
+    public ICommand NavigateCommand => new Command<string>(async (page) =>
+    {
+        switch (page)
+        {
+            case "Attention":
+                await this.Navigation.PushAsync(new AttentionHomePage());
+                break;
+            case "Organization":
+                await this.Navigation.PushAsync(new OrganizationHomePage());
+                break;
+            case "People":
+                await this.Navigation.PushAsync(new PeopleHomePage());
+                break;
+            case "Mindfulness":
+                await this.Navigation.PushAsync(new MindfulnessHomePage());
+                break;
+            case "GraphTasks":
+                await this.Navigation.PushAsync(new ProcrastinationHomePage());
+                break;
+        }
+    });
+    private void LoadFlashcards()
+    {
+        flashcards = AffirmationsRepository.GetFlashcards();
+        if (flashcards.Count > 0)
+        {
+            DisplayFlashcard(flashcards[0]);
+        }
+    }
+
+    private void DisplayFlashcard(Flashcard flashcard)
+    {
+        Flashcard1Front.Text = flashcard.Question;
+    }
+
+    private async void OnRandomButtonClicked(object sender, EventArgs e)
+    {
+        var randomIndex = random.Next(flashcards.Count);
+        DisplayFlashcard(flashcards[randomIndex]);
+    }
+}
